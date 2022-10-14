7ec08c9d83d4d967ddeecea6288a7e3272bfaf07
ArtemAmiragyan <artyom.amiragyan@yandex.ru>
2022-10-14 21:38:26 +0300

    test: github actions 4

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup lang="ts">
-import FrontLayout from '@/layouts/BaseFrontLayout.vue';
+import FrontLayout from '@layouts/BaseFrontLayout.vue';
 import { RouteMeta, useRoute } from 'vue-router';
 import { shallowRef, watch } from 'vue';
 
